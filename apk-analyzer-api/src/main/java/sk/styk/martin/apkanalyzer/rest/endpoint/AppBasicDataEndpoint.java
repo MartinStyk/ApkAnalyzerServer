0aceb30a728b8d89e1c092b5661961a9a831c0db
Martin Styk <martin.styk@gmail.com>
2017-11-14 09:41:05 +0100

    Adjust context paths

--- apk-analyzer-api/src/main/java/sk/styk/martin/apkanalyzer/rest/endpoint/AppBasicDataEndpoint.java
+++ apk-analyzer-api/src/main/java/sk/styk/martin/apkanalyzer/rest/endpoint/AppBasicDataEndpoint.java
@@ -14,7 +14,7 @@ import javax.ws.rs.core.Response;
 import java.util.List;
 
 
-@Path("/appdata/basic")
+@Path("/apps/basic")
 public class AppBasicDataEndpoint {
 
     @Inject
--- apk-analyzer-api/src/main/java/sk/styk/martin/apkanalyzer/rest/endpoint/AppDataEndpoint.java
+++ apk-analyzer-api/src/main/java/sk/styk/martin/apkanalyzer/rest/endpoint/AppDataEndpoint.java
@@ -16,7 +16,7 @@ import javax.ws.rs.core.Response;
 import java.util.List;
 
 
-@Path("/appdata")
+@Path("/apps")
 public class AppDataEndpoint {
 
     @Inject
