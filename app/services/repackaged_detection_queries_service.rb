ddf5876e9c0b9730e7c8bca5837bb49860853042
Martin Styk <martin.styk@gmail.com>
2017-12-08 22:03:50 +0100

    Repackaged detection service refactoring

---
+++ app/services/repackaged_detection_queries_service.rb
@@ -0,0 +1,37 @@
+class RepackagedDetectionQueriesService
+
+
+  # filter initial dataset based on metrics which are hard to obfuscate
+  # return ids of such app records
+  def filter_initial_dataset(app_record)
+    AppRecord.where(:number_xmls => (app_record.number_xmls * 0.8).round..(app_record.number_xmls * 1.2).round,
+                    :number_xmls_with_different_name => (app_record.number_xmls_with_different_name * 0.8).round..(app_record.number_xmls_with_different_name * 1.2).round,
+                    :number_pngs => (app_record.number_pngs * 0.8).round..(app_record.number_pngs * 1.2).round,
+                    :number_pngs_with_different_name => (app_record.number_pngs_with_different_name * 0.8).round..(app_record.number_pngs_with_different_name * 1.2).round,
+                    :number_files_total => (app_record.number_files_total * 0.8).round..(app_record.number_files_total * 1.2).round,
+                    :total_number_of_classes => (app_record.total_number_of_classes * 0.8).round..(app_record.total_number_of_classes * 1.2).round,
+                    :total_number_of_classes_without_inner_classes => (app_record.total_number_of_classes_without_inner_classes * 0.8).round..(app_record.total_number_of_classes_without_inner_classes * 1.2).round)
+                    .pluck(:id)
+  end
+
+  def drawable_intersect_query(id_1, id_2)
+    result = ActiveRecord::Base.connection.execute(" SELECT count(*)
+    FROM (
+             SELECT file_hash
+    FROM drawables
+    WHERE app_record_id = #{id_1}
+    INTERSECT
+    SELECT file_hash
+    FROM drawables
+    WHERE app_record_id = #{id_2}
+    );")
+    result[0][0]
+
+    # res_2 = (Drawable.where(app_record_id: id_1) & (Drawable.where(app_record_id: id_1))).size
+  end
+
+  def drawable_union_query(id_1, id_2)
+    Drawable.where(app_record_id: [id_1, id_2]).distinct('file_hash').count
+  end
+
+end
--- app/services/repackaged_detection_service.rb
+++ app/services/repackaged_detection_service.rb
@@ -1,31 +1,21 @@
 class RepackagedDetectionService
 
-
   def detection(app_record)
+    @query_service = RepackagedDetectionQueriesService.new
 
     # filter initial dataset based on metrics which are hard to obfuscate
-    candidate_ids = AppRecord.where(:number_xmls => (app_record.number_xmls * 0.8).round..(app_record.number_xmls * 1.2).round,
-                                    :number_xmls_with_different_name => (app_record.number_xmls_with_different_name * 0.8).round..(app_record.number_xmls_with_different_name * 1.2).round,
-                                    :number_pngs => (app_record.number_pngs * 0.8).round..(app_record.number_pngs * 1.2).round,
-                                    :number_pngs_with_different_name => (app_record.number_pngs_with_different_name * 0.8).round..(app_record.number_pngs_with_different_name * 1.2).round,
-                                    :number_files_total => (app_record.number_files_total * 0.8).round..(app_record.number_files_total * 1.2).round,
-                                    :total_number_of_classes => (app_record.total_number_of_classes * 0.8).round..(app_record.total_number_of_classes * 1.2).round,
-                                    :total_number_of_classes_without_inner_classes => (app_record.total_number_of_classes_without_inner_classes * 0.8).round..(app_record.total_number_of_classes_without_inner_classes * 1.2).round)
-                        .pluck(:id)
+    candidate_ids = @query_service.filter_initial_dataset app_record
 
     # find similar apps based on drawable similarity
-    repackaged_ids = []
-    candidate_ids.each do |candidate_id|
-      similarity_ratio_drawables = drawable_intersect_query(app_record.id, candidate_id) / drawable_union_query(app_record.id, candidate_id).to_f
-
-      if similarity_ratio_drawables > 0.8
-        repackaged_ids << candidate_id
-        SimilarAppRecord.find_or_create_by!(app_record: app_record, app_record_similar_id: candidate_id, score: similarity_ratio_drawables)
-      end
-    end
+    repackaged = find_similar  app_record, candidate_ids
 
     # create hash signature -> number of apps
-    repackaged = AppRecord.find(repackaged_ids)
+    signature_to_number_of_apps repackaged
+  end
+
+  private
+  
+  def signature_to_number_of_apps(repackaged)
     signatures = {}
     repackaged.each do |record|
       signatures[record.cert_md5] = [] if signatures[record.cert_md5].nil?
@@ -36,27 +26,18 @@ class RepackagedDetectionService
     signatures
   end
 
+  def find_similar(app_record, candidate_ids)
+    repackaged_ids = []
+    candidate_ids.each do |candidate_id|
+      similarity_ratio_drawables = @query_service.drawable_intersect_query(app_record.id, candidate_id) / @query_service.drawable_union_query(app_record.id, candidate_id).to_f
 
-  private
+      if similarity_ratio_drawables > 0.8
+        repackaged_ids << candidate_id
+        SimilarAppRecord.find_or_create_by!(app_record: app_record, app_record_similar_id: candidate_id, score: similarity_ratio_drawables)
+      end
+    end
 
-  def drawable_intersect_query(id_1, id_2)
-    result = ActiveRecord::Base.connection.execute(" SELECT count(*)
-    FROM (
-             SELECT file_hash
-    FROM drawables
-    WHERE app_record_id = #{id_1}
-    INTERSECT
-    SELECT file_hash
-    FROM drawables
-    WHERE app_record_id = #{id_2}
-    );")
-    result[0][0]
-
-    # res_2 = (Drawable.where(app_record_id: id_1) & (Drawable.where(app_record_id: id_1))).size
-  end
-
-  def drawable_union_query(id_1, id_2)
-    Drawable.where(app_record_id: [id_1, id_2]).distinct('file_hash').count
+    AppRecord.find(repackaged_ids)
   end
 
 end
