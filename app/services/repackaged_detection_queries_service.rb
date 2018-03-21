d08b84039aa919f0aee5eb00ee0edf3c10d33a05
Martin Styk <martin.styk@gmail.com>
2018-03-21 17:25:13 +0100

    In case of many similar apps, take last 300

--- app/services/repackaged_detection_queries_service.rb
+++ app/services/repackaged_detection_queries_service.rb
@@ -7,14 +7,15 @@ class RepackagedDetectionQueriesService
 
     delta = 0.2
 
-    AppRecord.where(:number_xmls => (app_record.number_xmls * (1 -delta)).round..(app_record.number_xmls * (1 +delta)).round,
-                    :number_xmls_with_different_name => (app_record.number_xmls_with_different_name * (1 -delta)).round..(app_record.number_xmls_with_different_name * (1 +delta)).round,
-                    :number_drawables_by_extension => (app_record.number_drawables_by_extension * (1 -delta)).round..(app_record.number_drawables_by_extension * (1 +delta)).round,
-                    :number_pngs_with_different_name => (app_record.number_pngs_with_different_name * (1 -delta)).round..(app_record.number_pngs_with_different_name * (1 +delta)).round,
-                    :number_files_total => (app_record.number_files_total * (1 -delta)).round..(app_record.number_files_total * (1 +delta)).round,
-                    :total_number_of_classes => (app_record.total_number_of_classes * (1 -delta)).round..(app_record.total_number_of_classes * (1 +delta)).round,
-                    :total_number_of_classes_without_inner_classes => (app_record.total_number_of_classes_without_inner_classes * (1 -delta)).round..(app_record.total_number_of_classes_without_inner_classes * (1 +delta)).round)
+    AppRecord.where(:number_xmls => (app_record.number_xmls * (1 - delta)).round..(app_record.number_xmls * (1 + delta)).round,
+                    :number_xmls_with_different_name => (app_record.number_xmls_with_different_name * (1 - delta)).round..(app_record.number_xmls_with_different_name * (1 + delta)).round,
+                    :number_drawables_by_extension => (app_record.number_drawables_by_extension * (1 - delta)).round..(app_record.number_drawables_by_extension * (1 + delta)).round,
+                    :number_pngs_with_different_name => (app_record.number_pngs_with_different_name * (1 - delta)).round..(app_record.number_pngs_with_different_name * (1 + delta)).round,
+                    :number_files_total => (app_record.number_files_total * (1 - delta)).round..(app_record.number_files_total * (1 + delta)).round,
+                    :total_number_of_classes => (app_record.total_number_of_classes * (1 - delta)).round..(app_record.total_number_of_classes * (1 + delta)).round,
+                    :total_number_of_classes_without_inner_classes => (app_record.total_number_of_classes_without_inner_classes * (1 - delta)).round..(app_record.total_number_of_classes_without_inner_classes * (1 + delta)).round)
         .pluck(:id, :package_name, :certificate_hash)
+        .last(400)
   end
 
   def drawable_intersect_query(id_1, id_2)
