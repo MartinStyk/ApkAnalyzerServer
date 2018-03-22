ecc19a057bcb0ab5eb67219f48da14fe42717d1e
Martin Styk <martin.styk@gmail.com>
2018-03-22 10:59:03 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20180321135230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "adminpack"
 
   create_table "app_records", force: :cascade do |t|
     t.integer "app_hash"
