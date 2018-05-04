5e7a45748300a20f95a92e2ff63db60061486e37
Martin Styk <martin.styk@gmail.com>
2018-05-04 20:22:29 +0200

    Limit data in usage controller

--- app/controllers/usage_controller.rb
+++ app/controllers/usage_controller.rb
@@ -13,9 +13,9 @@ class UsageController < ApplicationController
     @result[:app_records] = service.app_record_count
     @result[:different_devices] = service.different_devices_count
     @result[:different_apps] = service.different_apps_count
-    @result[:different_package_names] = service.different_package_names_count
-    @result[:different_package_name_and_version_count] = service.different_package_name_and_version_count
-    @result[:filtered_drawables_count] = service.filtered_drawables_count
+    # @result[:different_package_names] = service.different_package_names_count
+    # @result[:different_package_name_and_version_count] = service.different_package_name_and_version_count
+    # @result[:filtered_drawables_count] = service.filtered_drawables_count
     @result[:library_drawable_hashes_count] = service.library_drawable_hashes_count
 
     @result[:permissions] = service.permissions_count
